aa3f76b64d8ceb666578a384ee09a202bbd2fdec
peter-duongGhub <pduong197@icloud.com>
2024-06-28 20:20:16 +0100

    completed user inputs function to collect expense; name, amount, category, location, date and append userinput to txt file

--- expense_tracker/src/expenses.py
+++ expense_tracker/src/expenses.py
@@ -1,9 +1,13 @@
 class ExpenseTracker:
-    def __init__(self, name, category, location, date, time):
+    def __init__(self, name, amount, location, date, category ):
         self.name = name
-        self.category = category
+        self.amount = amount
         self.location = location
         self.date = date
-        self.time = time
+        self.category = category
+
+    def __repr__(self):
+        return f"Expense: {self.name}, {self.category}, Location: {self.location} Amount: {self.amount:.2f} Date: {self.date}"
+
 
     # def given_name(self):
--- expense_tracker/src/main.py
+++ expense_tracker/src/main.py
@@ -1,76 +1,92 @@
 from expenses import ExpenseTracker
+import datetime
+import json
+
 
 
 def main():
+    print("Welcome to Peter's Expense Tracker")
+    expense_file_list = "list_of_expenses.txt"
 
-
-    # print("Welcome to Peter's Expense Tracker! ")
-    # print("Please enter your expense name: ")
-    # print("Please enter your expense amount: ")
-    # print("Please select a category from the following options: ")
-
-    # print("\nExpense Tracker:")
-    # print("1. Add Expense(s) ")
-    # print("2. Remove Expense(s) ")
-    # print("3. View Expense(s) ")
-    # print("4. Edit Expense(s) ")
-    # print("5. Total sum of Expense(s) ")
-    # print("6. Save and Exit")
+    # Get user input for Expense(s) including: Name, Amount, Category, Location, Date
     expense = get_user_input()
+    
+    # Append the expense to a file
+    with open(expense_file_list, "a") as f:
+        f.write(f"\n{expense}")
 
-# Get User Input (includes: name, amount, category, location, time)
-    def get_user_input():
-
-        expense_name = input("Please enter an expense name: ")
-        if expense_name.isalpha() and (20 >= len(expense_name) > 0):
-            print(f"You have entered {expense_name}")
+    # Read list of total expenses
+    with open(expense_file_list, "r") as f:
+        read_list = f.read()
+        if len(expense_file_list) == 0:
+            print("Nothing in list")
         else:
-            print("Please type a valid name") 
-
-        expense_amount = float(input("Please enter an expense amount: "))
-        
-        if expense_amount == 0:
-            print("You cannot have an expense of 0 amount! Please try again: ")
-        else:
-            print(f"You have entered {expense_amount:.2f}")
-        
-        categories_expense = [
-            "Food", 
-            "Bills", 
-            "Rent", 
-            "Car", 
-            "Education", 
-            "Miscellaneous"
-            ]
-        
-        
-        while True:
-            print("Select a category from the following options: ")
-            for i, category in enumerate(categories_expense):
-                print(f"{i + 1}. {category}")
-
-            user_select = int(input("Your chosen category number: "))
-            if user_select not in range(1, (len(categories_expense) + 1)):
-                print("Please select a valid category number: ")
-                continue
-            else:
-                print(f"You have selected {user_select}: {category[i]} ")
-                new_expenses = ExpenseTracker()
-                return
-
-        expense_location = input("Please enter location of expense: ")
+            print(f"Here are a list of your expenses{read_list}")
 
 
+    # Remove files
 
-        expense_time = input("Please enter time of expense: ")
-
-
-        # expense_location = input("Please enter location details: ")
-        # expense_time = input("Please enter the time when expense occured: ")
 
     # if __name__ == "__main__":
     #     main()
+   
+def get_user_input():
 
-    get_user_input()
+    expense_name = input("Please enter an expense name: ")
+    if expense_name.isalpha() and (20 >= len(expense_name) > 0):
+        print(f"You have entered {expense_name}")
+        expense_name = expense_name.capitalize()
+    else:
+        print("Please type a valid name") 
 
+    expense_amount = float(input("Please enter an expense amount: "))
+        
+    if expense_amount == 0:
+        print("You cannot have an expense of 0 amount! Please try again: ")
+    else:
+        print(f"You have entered {expense_amount:.2f}")
+
+        expense_location = input("Please enter location of expense: ")
+        expense_location = expense_location.upper()
+        if not expense_location.isalpha():
+            print("Please enter a valid location: ")
+        else:
+            print(f"You have entered {expense_location}")
+
+        expense_date = (input("Please enter a date in format YYYY-MM-DD: "))
+        if datetime.date.fromisoformat(expense_date):
+            print(f"You have entered {expense_date}")
+        else:
+            ("Invalid date entered, please enter a valid date!")
+
+        categories_expense = [
+        "Food", 
+        "Bills", 
+        "Rent", 
+        "Car", 
+        "Education", 
+        "Miscellaneous"
+            ]
+        
+        
+    while True:
+        print("Select a category from the following options: ")
+        for i, category in enumerate(categories_expense):
+            print(f"{i + 1}. {category}")
+        user_select = int(input("Your chosen category number: "))
+
+        if user_select not in range(1, (len(categories_expense) + 1)):
+            print("Please select a valid category number: ")
+            continue
+            
+        else:
+            print(f"You have selected {user_select}: {categories_expense[user_select - 1]} ")
+            category_selection = categories_expense[(user_select) - 1]
+            new_expense_object = ExpenseTracker(name=expense_name, amount=expense_amount, category=category_selection, location=expense_location, date=expense_date
+                )
+            return new_expense_object
+            
+
+        # expense_location = input("Please enter location details: ")
+        # expense_time = input("Please enter the time when expense occured: ")
 main()
