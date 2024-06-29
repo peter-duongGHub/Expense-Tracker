eaa11ac1b6653ad80078a7b3ba54751ebb009e00
peter-duongGhub <pduong197@icloud.com>
2024-06-30 00:57:29 +0100

    implemented DRY coding principle, removed redundant file path variables and replaced with 1 file path argument for some functions (add, view, remove expenses)

--- src/main.py
+++ src/main.py
@@ -13,7 +13,6 @@ def main():
 
 
 
-    # save_expense(expense, FILE_PATH)
 
 
     # Get user input for Expense(s) including: Name, Amount, Category, Location, Date    
@@ -23,11 +22,13 @@ def main():
     # Read list of total expenses
 
 def sub_menu():
+    expense_file_path = "expenses_list.txt"
     while True:
         user_selection = int(input("Your selection option please: "))
         if user_selection in range(1, 7):
             if user_selection == 1:
                 print("Starting your Expense Tracking Journey...")
+                add_budget()
                 print("What would you like to do first?\n")
                 print("Menu: ")
                 print("1. Add & Save Expense to Text File")
@@ -35,22 +36,20 @@ def sub_menu():
                 print("3. Remove an Expense")
                 print("4. Total Expenses by Category")
                 print("5. Return to main menu\n")
-            sub_selection = int(input("Make a choice (1-6): "))
+            sub_selection = int(input("Make a choice (1-5): "))
             if sub_selection == 1:
-                add_expense()
+                add_expense(expense_file_path)
             elif sub_selection == 2:
-                view_expense()   
+                view_expenses(expense_file_path)   
             elif sub_selection == 3:
-                remove_expense()
+                remove_expense(expense_file_path)
             elif sub_selection == 4:
                 pass
             elif sub_selection == 5:
-                pass
-            elif sub_selection == 6:
-                pass
-            # elif sub_selection == 4:
-            # elif sub_selection == 4:
-            #     save_expense(expense, FILE_PATH)
+                print("-------------")
+                return f"{main_menu()}"
+
+    
 
           
             return
@@ -67,22 +66,22 @@ def main_menu():
     print("3. View Instructions")
     print("4. Exit Program\n")
 
-def add_income(input):
-    income = 0
-    income += input
-    new_income = re.sub(r'(\d{3})(?=\d)' , r'\1,', str(income)[::-1])[::-1]
+def add_budget():
+    user_budget = int(input("Please enter your budget: "))
+    new_budget = re.sub(r'(\d{3})(?=\d)' , r'\1,', str(user_budget)[::-1])[::-1]
     
-    with open("income_list.txt", 'a+') as f:
-        f.write(f"${new_income}\n")
-
-    return new_income
+    with open("budget.txt", 'a+') as f:
+        f.write(f"${new_budget}\n")
+    print(f"You have entered ${new_budget}")
+    print("Your budget has been added to a list!")
+    return new_budget
 
 def total_expenses():
+    # with open("")
     pass
     
-def view_expense():
-    FILE_PATH = "./expenses_list.txt"
-    if os.path.exists(FILE_PATH):
+def view_expenses(file_path):
+    if os.path.exists(file_path):
         view_expense = input("Would you like to view your expense entries? (yes/no): ")
         if view_expense.isalpha() and view_expense.lower() == "yes":  
             with open("expenses_list.txt", "r") as f:
@@ -103,15 +102,14 @@ def view_expense():
     # if __name__ == "__main__":
     #     main()
 
-def remove_expense():
-        FILE_PATH = "./expenses_list.txt"
-        if os.path.exists(FILE_PATH):
-            with open(FILE_PATH, 'r') as f:
+def remove_expense(file_path):
+        if os.path.exists(file_path):
+            with open(file_path, 'r') as f:
                 lines = f.readlines()
-                view_expense()
+                view_expenses()
                 delete_line = int(input("Which expense entry would you like to delete? "))
                 lineindex = 1
-                with open(FILE_PATH, 'w') as f:
+                with open(file_path, 'w') as f:
                     for line in lines:
                         if lineindex != delete_line:
                             f.write(line)
@@ -122,12 +120,12 @@ def remove_expense():
         else:
             print("You do not currently have any expense entries to remove! ")
 
-def save_expense(new_expense_object:ExpenseTracker, FILE_PATH):
-    print(f"Saving your expense.... {new_expense_object} to {FILE_PATH}")
-    with open(FILE_PATH, "a+") as f:
+def save_expense(new_expense_object:ExpenseTracker, file_path):
+    print(f"Saving your expense.... {new_expense_object} to {file_path}")
+    with open(file_path, "a+") as f:
         f.write(f"{new_expense_object.name}, {new_expense_object.amount:.2f}, {new_expense_object.location}, {new_expense_object.date}, {new_expense_object.category}\n")
 
-def add_expense():
+def add_expense(file_path):
     count = 0
     expense_name = input("Please enter an expense name: ")
     if expense_name.isalpha() and (20 >= len(expense_name) > 0):
@@ -182,8 +180,8 @@ def add_expense():
             new_expense_object = ExpenseTracker(name=expense_name, amount=expense_amount, category=category_selection, location=expense_location, date=expense_date
                 )
             count += 1
-            FILE_PATH = "expenses_list.txt"
-            save_expense(new_expense_object, FILE_PATH)
+            
+            save_expense(new_expense_object, file_path)
             
             return
             
