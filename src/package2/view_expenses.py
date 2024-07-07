402bcd38fa2cdd49dd35e9229a6fb47cb59c608b
peter-duongGhub <pduong197@icloud.com>
2024-07-07 19:57:50 +0100

    finish adding comments to view expenses file

--- src/package2/view_expenses.py
+++ src/package2/view_expenses.py
@@ -5,14 +5,20 @@ from rich import print
 from rich.console import Console
 from rich.theme import Theme
 
+# Initialise console variable to use Rich Package installed from Pip
 console = Console()
+
+# Apply Theme colour to display success or error messages and initialise this to error for use as "error.print"
 custom_theme = Theme({"success": "green", "error": "red"})
 error = Console(theme=custom_theme)
 
-
+# Define view expenses function
 def view_expenses(file_path):
+    # Using os module check file exists
     if os.path.exists(file_path):
+        # Check contents of file
         checkfile = os.stat(file_path).st_size
+        # While True loop to ensure file size is not 0 and user enters correct input
         while True:
             if checkfile == 0:
                 error.print(
@@ -38,6 +44,7 @@ def view_expenses(file_path):
                 else:
                     error.print("Please enter a valid answer", style="error")
                     continue
+    #Else statement terminal output error message if file doesnt exist 
     else:
         error.print("You currently have no expense entries", style="error")
         return
